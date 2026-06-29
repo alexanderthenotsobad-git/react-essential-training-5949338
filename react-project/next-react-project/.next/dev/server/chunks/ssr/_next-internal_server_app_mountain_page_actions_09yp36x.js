8909cb23a498fcf1c6bcfd4fcdc8816903ba8629
Alex <alex.gomez.usf@outlook.com>
2026-06-29 19:24:59 +0000

    chore: ignore .next build directory

--- react-project/next-react-project/.next/dev/server/chunks/ssr/_next-internal_server_app_mountain_page_actions_09yp36x.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/mountain/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_mountain_page_actions_09yp36x.js.map
--- react-project/next-react-project/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gZt93Bcwxyx/DO+H7gCnzYiRDrpRx3UgGy7BJ5UeVJE=\"\n}"
